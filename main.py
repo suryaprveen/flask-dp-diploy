f08e907811772290acfce54d6a8b3092f40a2b71
Suryapraveen <suryapraveen23@gmail.com>
2020-04-25 21:33:40 +0530

    deploy

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ if ENV == 'dev':
 
 else:
     app.debug = False
-    app.config[ 'SQLALCHEMY_DATABASE_URI' ]=' postgres://jfrjcnkutalcef:72270166de3c5782bf3abdfb677f039558f1c4d21d53b6758c51b8f96424ed49@ec2-3-223-21-106.compute-1.amazonaws.com:5432/d3v7gj3hjsf4k8'
+    app.config[ 'SQLALCHEMY_DATABASE_URI' ]='postgres://jfrjcnkutalcef:72270166de3c5782bf3abdfb677f039558f1c4d21d53b6758c51b8f96424ed49@ec2-3-223-21-106.compute-1.amazonaws.com:5432/d3v7gj3hjsf4k8'
 
 app.config[ 'SECRET_KEY' ] = 'Thisissupposedtobesecret!'
 
